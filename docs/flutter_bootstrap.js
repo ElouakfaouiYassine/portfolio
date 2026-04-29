cf693c447e5715f771bd53eac05caeeeb26b2240
ElouakfaouiYassine <ElouakfaouiYassine@users.noreply.github.com>
2026-04-29 00:34:47 +0000

    ci: deploy web to docs [skip ci]

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "21251659"
+    serviceWorkerVersion: "1666492727"
   }
 });
